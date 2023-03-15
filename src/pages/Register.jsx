1000f3578fce93be4bd6d7ccc635389d9f703cf7
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-15 21:36:30 +0300

    initial & folder structure

---
+++ src/pages/Register.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <form>
+            <input type="text" />
+            <input type="text" />
+            <input type="email" />
+            <input type="password" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
