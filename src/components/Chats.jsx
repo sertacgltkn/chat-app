ec7eeb53849edfac2a8100665843b59f44ceefca
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-16 18:34:20 +0300

    chat & message

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+       <div className="userChat">
+        <img src="https://images.pexels.com/photos/14691986/pexels-photo-14691986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/14691986/pexels-photo-14691986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <span>
+          Just Now
+        </span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/14691986/pexels-photo-14691986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+      </div>
+    </div>
   )
 }
 
